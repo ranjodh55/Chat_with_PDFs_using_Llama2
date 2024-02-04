97586ef2e4c7cf499e192d68c4dc51dd8de0cb0c
Ranjodh <mrsmileisgod@gmail.com>
2024-02-05 00:06:01 +0530

    Put everything on AWS instead of local

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
-
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import json
 import os
 import pinecone
 import time
-from torch import cuda, bfloat16
-import transformers
-from langchain.llms import HuggingFacePipeline
-from langchain.vectorstores import Pinecone
-from langchain.chains import RetrievalQA
+import pandas as pd
+import requests
 from dotenv import load_dotenv
 import streamlit as st
+from typing import List
+import numpy as np
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
-
+#
 hf_token = os.getenv('HF_TOKEN')
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
 pinecone_env = os.getenv('PINECONE_ENV')
-
-
-def get_hf_embedding():
-    embed_model_id = 'sentence-transformers/all-MiniLM-L6-v2'
-
-    device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
-
-    embed_model = HuggingFaceEmbeddings(
-        model_name=embed_model_id,
-        model_kwargs={'device': device},
-        encode_kwargs={'device': device, 'batch_size': 32}
-    )
-    return embed_model
 
 
 def get_pinecone_index():
@@ -56,100 +41,115 @@ def get_pinecone_index():
         data = loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
         docs = text_splitter.split_documents(data)
-        embed_model = get_hf_embedding()
-        docsearch = Pinecone.from_texts([t.page_content for t in docs], embed_model, index_name=index_name)
+        docs = [doc.page_content for doc in docs]
+        df = pd.DataFrame(docs, columns=["text"])
+
+        from tqdm.auto import tqdm
+
+        batch_size = 2  # can increase but needs larger instance size otherwise instance runs out of memory
+        vector_limit = 1000
+
+        pdf_df = df[:vector_limit]
+        index = pinecone.Index(index_name)
+
+        for i in tqdm(range(0, len(pdf_df), batch_size)):
+            # find end of batch
+            i_end = min(i + batch_size, len(pdf_df))
+            # create IDs batch
+            ids = [str(x) for x in range(i, i_end)]
+            # create metadata batch
+            metadatas = [{'text': text} for text in pdf_df["text"][i:i_end]]
+            # create embeddings
+            texts = pdf_df["text"][i:i_end].tolist()
+            embeddings = embed_docs(texts)
+            # create records list for upsert
+            records = zip(ids, embeddings, metadatas)
+            # upsert to Pinecone
+            index.upsert(vectors=records)
 
     index = pinecone.Index(index_name)
 
     return index
 
 
-def get_llm():
-    model_id = 'meta-llama/Llama-2-13b-chat-hf'
+# hit the embedding model endpoint
+def embed_docs(docs: List[str]):
+    url = "https://ues5110i6e.execute-api.us-east-1.amazonaws.com/prod"
+    payload = json.dumps({
+        "inputs": docs,
+    })
 
-    device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
-
-    # set quantization configuration to load large model with less GPU memory
-    # this requires the `bitsandbytes` library
-    bnb_config = transformers.BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type='nf4',
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=bfloat16
-    )
-
-    # begin initializing HF items, need auth token for these
-    hf_auth = hf_token
-    model_config = transformers.AutoConfig.from_pretrained(
-        model_id,
-        use_auth_token=hf_auth
-    )
-
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_id,
-        trust_remote_code=True,
-        config=model_config,
-        quantization_config=bnb_config,
-        device_map='auto',
-        use_auth_token=hf_auth
-    )
-    model.eval()
-    print(f"Model loaded on {device}")
-
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        model_id,
-        use_auth_token=hf_auth
-    )
-
-    generate_text = transformers.pipeline(
-        model=model, tokenizer=tokenizer,
-        return_full_text=True,  # langchain expects the full text
-        task='text-generation',
-        # we pass model parameters here too
-        temperature=0.0,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-        max_new_tokens=512,  # mex number of tokens to generate in the output
-        repetition_penalty=1.1  # without this output begins repeating
-    )
-
-    llm = HuggingFacePipeline(pipeline=generate_text)
-
-    return llm
+    out = requests.post(url, payload, headers={"content-type": 'application/json', 'x-api-key': ''})
+    array = np.array(out.json())
+    embeddings = np.mean(np.array(array), axis=1)
+    return embeddings.tolist()
 
 
-def get_response(question):
-    text_field = 'text'  # field in metadata that contains text content
+def construct_context(contexts: List[str]) -> str:
+    chosen_sections = []
 
+    for text in contexts:
+        text = text.strip()
+        chosen_sections.append(text)
+    concatenated_doc = "\n".join(chosen_sections)
+    return concatenated_doc
+
+
+def rag_query(question: str) -> str:
+    # create query vec
+    query_vec = embed_docs(question)[0]
+    # query pinecone
     index = get_pinecone_index()
-    embed_model = get_hf_embedding()
-    llm = get_llm()
-    vectorstore = Pinecone(
-        index, embed_model.embed_query, text_field
-    )
+    res = index.query(vector=query_vec, top_k=5, include_metadata=True)
+    # get contexts
+    contexts = [match.metadata['text'] for match in res.matches]
+    # build the multiple contexts string
+    context_str = construct_context(contexts=contexts)
 
-    rag_pipeline = RetrievalQA.from_chain_type(
-        llm=llm, chain_type='stuff',
-        retriever=vectorstore.as_retriever()
-    )
+    prompt_template = """Answer the following QUESTION in a detailed way based only on the BOOK
+    given. Do not add any unnessary information. If you do not know the answer and the BOOK doesn't
+    contain the answer truthfully say "I don't know".
 
-    response = rag_pipeline(f"""
-         Answer the question asked in a very detailed manner from the provided book. 
-         If the answer is not found in the book say "The book does not contain information about this". 
-         Do not provide wrong answers.
-         Question: \n{question}\n
-         Answer:
-    """)
-    return response['result']
+    BOOK:
+    {context}
+
+    QUESTION:
+    {question}
+
+    ANSWER:
+    """
+
+    text_input = prompt_template.replace("{context}", context_str).replace("{question}", question)
+    # hit the llama2 endpoint
+    url = "https://r9dgkrfhk7.execute-api.us-east-1.amazonaws.com/prod"
+    payload = json.dumps({
+        "inputs": text_input,
+        "parameters": {
+            "do_sample": True,
+            "top_p": 0.5,
+            "temperature": 0.1,
+            "top_k": 50,
+            "max_new_tokens": 1024,
+            "stop": [
+                "</s>"
+            ]
+        }
+    })
+
+    response = requests.post(url, payload, headers={"content-type": 'application/json', 'x-api-key': ''})
+    answer = response.json()[0]['generated_text'].replace(text_input, '')
+    return answer
 
 
-def main():
+if __name__ == "__main__":
     # Set page title and header
     st.set_page_config(page_title="PDF Chatbot", page_icon="🤖")
-    st.title("Chat with a PDF using Google's Gemini Pro Model")
+    st.title("Chat with a PDF using using Meta's Llama2 Model")
 
     # Add a description
     st.write(
-        "This chatbot allows you to interact with a PDF document using Google's Gemini Pro model. You can ask "
-        "questions about the document, or even have a conversation with it."
+        "This chatbot allows you to interact with a PDF document using Meta's Llama2 model. You can ask "
+        "questions about the document."
     )
 
     # Create a text input field for the user to enter their message
@@ -163,16 +163,13 @@ def main():
 
     # Define the function to send the user's message to the chatbot and display the response
     def send_message(user_input):
-        # Use the Google Gemini Pro model to generate a response to the user's message
-        response = get_response(user_input)
+        # Use the Llama2 model to generate a response to the user's message
+        response = rag_query(user_input)
 
         # Display the chatbot's response in the text area
         chatbot_response.markdown(response)
 
+
     # Send the user's message to the chatbot when the send button is clicked
     if send_button:
         send_message(user_input)
-
-
-if __name__ == "__main__":
-    main()
